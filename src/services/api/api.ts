462aba5928c84be59cf298fe60d0d3cd76eaddfe
VitaliyPanchishniy <144204960+VitaliyPanchishniy@users.noreply.github.com>
2025-04-03 02:06:55 +0300

    update main

---
+++ src/services/api/api.ts
@@ -0,0 +1,18 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Post } from './types';
+
+// Define a service using a base URL and expected endpoints
+export const catClickerApi = createApi({
+  reducerPath: 'catClickerApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com' }),
+  endpoints: (builder) => ({
+    getAllPosts: builder.query<Post, string>({
+      query: () => `/post`,
+    }),
+  }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetAllPostsQuery } = catClickerApi;
